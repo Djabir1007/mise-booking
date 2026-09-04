073112763fcad311993e0c13b00f7a36fc815f8e
Djabir1007 <jabir.pashaev@gmail.com>
2026-09-04 21:20:32 +0300

    Добавлен экран подтверждения и адаптив

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Container from "@/shared/ui/Container/Container";
 
 import styles from "./page.module.css";
 import BookingForm from "@/components/BookingForm/BookingForm";
+import ConfirmationScreen from "@/components/ConfirmationScreen/ConfirmationScreen";
 
 export default function Home() {
   return (
